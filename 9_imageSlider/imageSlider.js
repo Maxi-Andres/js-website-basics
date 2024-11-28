1e9016017f5f6bc949706b769ecf68a9d5ae63fe
Maxi <machugab@gmail.com>
2024-11-28 11:38:38 -0300

    small things

--- 9_imageSlider/imageSlider.js
+++ 9_imageSlider/imageSlider.js
@@ -1,5 +1,7 @@
+// image slider
+
 const slides = document.querySelector('.slides');
-const images = document.querySelectorAll('.slides img');
+const images = document.querySelectorAll('.slides img'); // NodeList (coleccion de nodos en este caso imagenes)
 let currentIndex = 0;
 
 document.getElementById('next').addEventListener('click', () => {
@@ -13,5 +15,17 @@ document.getElementById('prev').addEventListener('click', () => {
 });
 
 function updateSlider() {
-    slides.style.transform = `translateX(-${currentIndex * 600}px)`;
+    slides.style.transform = `translateX(-${currentIndex * 600}px)`; //600px porque es el ancho de cada imagen
 }
+
+// La propiedad transform de CSS permite aplicar transformaciones visuales a un elemento HTML, como rotar, escalar, inclinar o mover 
+// (trasladar) un elemento en el espacio 2D o 3D.
+// translateX mueve el elemento horizontalmente.
+
+//* Valor positivo (translateX(100px)): mueve el elemento hacia la derecha.
+//* Valor negativo (translateX(-100px)): mueve el elemento hacia la izquierda.
+
+// Inicio (currentIndex = 0): translateX(0px)
+// Siguiente (currentIndex = 1): translateX(-600px)
+// Siguiente (currentIndex = 2): translateX(-1200px)
+// Siguiente (currentIndex = 0): regresa al inicio (translateX(0px)).
